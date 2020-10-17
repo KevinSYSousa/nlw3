bd742244a13c795f0f031be33bca443bf3e0a995
KevinSYSousa <kevinsousaec@outlook.com>
2020-10-17 16:15:22 -0300

    day 4

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-console.log("teste");
+//LIB IMPORT
+const express = require("express");
+const path = require("path");
+const pages = require("./pages.js");
+
+//START EXPRESS
+const server = express();
+
+//ROUTE CREATION
+server
+  .use(express.static("public"))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "hbs")
+  .get("/", pages.index)
+  .get("/orphanage", pages.orphanage)
+  .get("/orphanages", pages.orphanages)
+  .get("/create-orphanage", pages.createOrphanage);
+
+//START SERVER
+server.listen(5500);
